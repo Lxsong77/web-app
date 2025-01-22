ba906df3158c4887c380ee5fb7bbd7f90ec14d15
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-01-22 16:59:48 -0500

    add sidebar

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -25,85 +25,85 @@ export default function Dashboard() {
             <div>
               <h5> CS5001 Intensive Foundations of CS </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                learn Python  </p>
               <button> Go </button>
             </div>
           </Link>
         </div>
 
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
+          <Link to="/Kambaz/Courses/5002/Home"
                 className="wd-dashboard-course-link" >
-            <img src="images/reactjs.jpg" width={200} />
+            <img src="images/5002.jpg" width={200} />
             <div>
-              <h5> CS1234 React JS </h5>
+              <h5> CS5002 Discrete Structures</h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                  Learn Math</p>
               <button> Go </button>
             </div>
           </Link>
         </div>
 
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
+          <Link to="/Kambaz/Courses/5003/Home"
                 className="wd-dashboard-course-link" >
-            <img src="images/reactjs.jpg" width={200} />
+            <img src="images/5003.jpg" width={200} />
             <div>
-              <h5> CS1234 React JS </h5>
+              <h5> CS5003  Recition fro CS5001 </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+              Pracitice  </p>
               <button> Go </button>
             </div>
           </Link>
         </div>
 
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
+          <Link to="/Kambaz/Courses/5004/Home"
                 className="wd-dashboard-course-link" >
-            <img src="images/reactjs.jpg" width={200} />
+            <img src="images/5004.jpg" width={200} />
             <div>
-              <h5> CS1234 React JS </h5>
+              <h5> CS5004: Object-Oriented Design </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                Learn JAVA  </p>
               <button> Go </button>
             </div>
           </Link>
         </div>
 
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
+          <Link to="/Kambaz/Courses/5005/Home"
                 className="wd-dashboard-course-link" >
-            <img src="images/reactjs.jpg" width={200} />
+            <img src="images/5005.jpg" width={200} />
             <div>
-              <h5> CS1234 React JS </h5>
+              <h5> CS5005 Algorithm </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                Learn Algorithm  </p>
               <button> Go </button>
             </div>
           </Link>
         </div>
 
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
+          <Link to="/Kambaz/Courses/5006/Home"
                 className="wd-dashboard-course-link" >
-            <img src="images/reactjs.jpg" width={200} />
+            <img src="images/5006.jpeg" width={200} />
             <div>
-              <h5> CS1234 React JS </h5>
+              <h5> CS5006 Computer system </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                Learn foundation </p>
               <button> Go </button>
             </div>
           </Link>
         </div>
 
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
+          <Link to="/Kambaz/Courses/5007/Home"
                 className="wd-dashboard-course-link" >
-            <img src="images/reactjs.jpg" width={200} />
+            <img src="images/5007.jpeg" width={200} />
             <div>
-              <h5> CS1234 React JS </h5>
+              <h5> CS5007 Data Structure </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                Management Data  </p>
               <button> Go </button>
             </div>
           </Link>
