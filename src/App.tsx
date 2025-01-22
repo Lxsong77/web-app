f53f0e9e4ef9f70f440980a63074543499ecb7ea
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-01-22 14:11:49 -0500

    add course

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Labs from "./Labs";
+import Kambaz from "./Kambaz";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App() {
@@ -8,8 +9,9 @@ export default function App() {
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/" element={<Navigate to="Kambaz" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
         </Routes>
       </div>
     </HashRouter>
