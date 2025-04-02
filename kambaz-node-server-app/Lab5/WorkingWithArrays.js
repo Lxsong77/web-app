2f959470f4235128c903215343a1eac35152ebdd
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-02 17:39:02 -0400

    finish 5.2

--- kambaz-node-server-app/Lab5/WorkingWithArrays.js
+++ kambaz-node-server-app/Lab5/WorkingWithArrays.js
@@ -24,6 +24,13 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(todos);
       });
+    
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body,  id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+      });
+
 
     app.get("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
@@ -31,12 +38,41 @@ export default function WorkingWithArrays(app) {
         res.json(todo);
       });
 
+    app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+    todos = todos.map((t) => {
+        if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+        }
+        return t;
+    });
+    res.sendStatus(200);
+    });
+        
+
     app.get("/lab5/todos/:id/delete", (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
         todos.splice(todoIndex, 1);
         res.json(todos);
     });
+
+    app.delete("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+            return;
+          }      
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+      });
+    
     
     app.get("/lab5/todos/:id/title/:title", (req, res) => {
         const { id, title } = req.params;
@@ -66,4 +102,6 @@ export default function WorkingWithArrays(app) {
             res.status(404).send("Todo not found");
         }
     });
+
+    
 };
--- src/Labs/Lab5/HttpClient.tsx
+++ src/Labs/Lab5/HttpClient.tsx
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+
 import * as client from "./client";
 
-const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 
 export default function HttpClient() {
   const [welcomeOnClick, setWelcomeOnClick] = useState("");
+  const [welcomeOnLoad, setWelcomeOnLoad] = useState("");
 
   const fetchWelcomeOnClick = async () => {
     const message = await client.fetchWelcomeMessage();
     setWelcomeOnClick(message);
-
-    const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
-    setWelcomeOnClick(response.data);
   };
+  const fetchWelcomeOnLoad = async () => {
+    const welcome = await client.fetchWelcomeMessage();
+    setWelcomeOnLoad(welcome);
+  };
+  useEffect(() => {
+    fetchWelcomeOnLoad();
+  }, []);
+
   return (
     <div>
       <h3>HTTP Client</h3> <hr />
@@ -22,6 +27,11 @@ export default function HttpClient() {
         Fetch Welcome
       </button> <br />
       Response from server: <b>{welcomeOnClick}</b>
+      <hr />
+
+      <h4>Requesting on Load</h4>
+        Response from server: <b>{welcomeOnLoad}</b>
+      <hr />
     </div>
   );
 }
