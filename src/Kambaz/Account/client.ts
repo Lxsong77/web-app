7b569343c78b1f75f6d003dbab36c5323cc4143a
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-13 20:16:42 -0400

    1

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -7,7 +7,7 @@ export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const signin = async (credentials: any) => {
     const response = await axiosWithCredentials.post( `${USERS_API}/signin`, credentials );
     return response.data;
-};
+}; 
 
 export const signup = async (user: any) => {
     const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
